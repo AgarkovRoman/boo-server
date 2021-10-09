c66813cb052661e78f875e48c4051d4e974966a4
Roman Agarkov <agarkovromachka@mail.ru>
2021-10-09 16:59:37 +0300

    deploy

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -1,26 +1,26 @@
 import { ConfigService } from '@nestjs/config';
+import { MongoClient } from 'mongodb';
 import { TypegooseModuleOptions } from 'nestjs-typegoose';
 
-export const getMongoConfig = async (
-  configService: ConfigService,
-): Promise<TypegooseModuleOptions> => {
-  return {
-    uri: getMongoUri(configService),
-    ...getMongoOptions(),
-  };
+// export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
+//   const client = new MongoClient(getMongoUri(configService));
+//
+//   return {
+//     uri: client.get(),
+//     ...getMongoOptions();
+//   };
+// };
+
+export const getMongoConfig = (configService: ConfigService) => {
+  return new MongoClient(getMongoUri(configService));
 };
 
-const getMongoUri = (configService: ConfigService) =>
-  'mongodb://' +
-  configService.get('MONGO_LOGIN') +
-  ':' +
+export const getMongoUri = (configService: ConfigService) =>
+  'mongodb+srv://boo-boo:' +
   configService.get('MONGO_PASSWORD') +
-  '@' +
-  configService.get('MONGO_HOST') +
-  ':' +
-  configService.get('MONGO_PORT') +
-  '/' +
-  configService.get('MONGO_AUTHDATABASE');
+  '@cluster0.dqwhz.mongodb.net/' +
+  configService.get('MONGO_DB_NAME') +
+  '?retryWrites=true&w=majority';
 
 const getMongoOptions = () => ({
   useNewUrlParser: true,
